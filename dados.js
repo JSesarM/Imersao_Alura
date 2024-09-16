8be989b1b62436a49f63f279846f2770b4920d35
JSesarM <149620714+JSesarM@users.noreply.github.com>
2024-09-16 18:24:22 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -4,7 +4,7 @@ let dados = [
     descricao : "O Sport Club Corinthians Paulista, comumente referido simplesmente como Corinthians ou Timão, é uma das mais tradicionais e populares instituições esportivas do Brasil, com sede na cidade de São Paulo. Fundado em 1º de setembro de 1910, o clube possui uma vasta história marcada por conquistas nacionais e internacionais, além de uma torcida apaixonada e fiel, conhecida como Fiel Torcida",
     link : "https://pt.wikipedia.org/wiki/Sport_Club_Corinthians_Paulista",
     competicoes : "Atualmente o Corinthians disputa: A quartas de finais das Copas do brasil e Sulamericana, além da Série A do Campeonato Brasileiro.",
-    tags : "Timão Fiel Coringão"
+    tags : "Timão, Fiel, Coringão, SCCP"
     },
 
     {
@@ -12,17 +12,154 @@ let dados = [
         descricao: "O Sociedade Esportiva Palmeiras, popularmente conhecido como Palmeiras, é um dos clubes de futebol brasileiro com sede na cidade de São Paulo. Fundado em 14 de agosto de 1914, o Palmeiras é um dos clubes mais populares do Brasil e possui uma vasta história marcada por conquistas nacionais e internacionais.",
         link: "https://pt.wikipedia.org/wiki.org/wiki/Sociedade_Esportiva_Palmeiras",
         competicoes: "Atualmente o Palmeiras disputa: O Campeonato Brasileiro e a Libertadores da América.",
-        tags : "Verdão Porco"
+        tags : "Verdão, Porco, SEP"
       },
 
       {
-        clube : "Santos",
-        descricao: "O Santos Futebol Clube, popularmente conhecido como Santos FC ou simplesmente Santos, é um clube de futebol brasileiro com sede na cidade de Santos, no litoral do estado de São Paulo. Fundado em 14 de abril de 1912, o Santos é um dos clubes mais tradicionais do Brasil e possui uma vasta história marcada por conquistas nacionais e internacionais.",
-        link: "https://pt.wikipedia.org/wiki/Santos_Futebol_Clube",
-        competicoes: "Atualmente o Santos disputa: O Campeonato Brasileiro e a Copa do Brasil.",
-        tags : "Peixe Peixão"
-      }
+        clube: "São Paulo",
+        descricao: "O São Paulo Futebol Clube, popularmente conhecido como São Paulo FC ou simplesmente São Paulo, é um clube de futebol brasileiro com sede na cidade de São Paulo. Fundado em 25 de janeiro de 1930, o São Paulo é um dos clubes mais populares do Brasil e possui uma vasta história marcada por conquistas nacionais e internacionais, incluindo três títulos da Copa Libertadores da América e três títulos do Mundial de Clubes da FIFA.",
+        link: "https://pt.wikipedia.org/wiki/São_Paulo_Futebol_Clube",
+        competicoes: "Atualmente o São Paulo disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Paulista.",
+        tags: "Tricolor, Morumbi, Soberano, SPFC"
+    },
 
+
+    {
+      clube: "Flamengo",
+      descricao: "O Clube de Regatas do Flamengo, popularmente conhecido como Flamengo, é um clube poliesportivo brasileiro com sede na cidade do Rio de Janeiro. Fundado em 1895, é um dos clubes mais populares do Brasil e possui uma vasta história marcada por conquistas nacionais e internacionais, especialmente no futebol.",
+      link: "https://pt.wikipedia.org/wiki/Clube_de_Regatas_do_Flamengo",
+      competicoes: "Atualmente o Flamengo disputa: O Campeonato Brasileiro, a Copa do Brasil, o Campeonato Carioca e competições internacionais como a Copa Libertadores da América.",
+      tags: "Mengão, Nação, Rubro-Negro"
+  },
+  
+  {
+      clube: "Vasco da Gama",
+      descricao: "O Club de Regatas Vasco da Gama, popularmente conhecido como Vasco, é um clube poliesportivo brasileiro com sede na cidade do Rio de Janeiro. Fundado em 21 de agosto de 1898, o Vasco é um dos clubes mais tradicionais do Brasil e possui uma grande torcida.",
+      link: "https://pt.wikipedia.org/wiki/Club_de_Regatas_Vasco_da_Gama",
+      competicoes: "Atualmente o Vasco disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Carioca.",
+      tags: "Gigante da Colina, Cruzmaltino"
+  },
+  
+  {
+      clube: "Fluminense Football Club",
+      descricao: "O Fluminense Football Club, popularmente conhecido como Fluminense, é um clube poliesportivo brasileiro com sede na cidade do Rio de Janeiro. Fundado em 21 de julho de 1902, o Fluminense é um dos clubes mais tradicionais do Brasil e possui uma grande torcida.",
+      link: "https://pt.wikipedia.org/wiki/Fluminense_Football_Club",
+      competicoes: "Atualmente o Fluminense disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Carioca.",
+      tags: "Tricolor, Fluzão"
+  },
+  
+  {
+      clube: "Botafogo de Futebol e Regatas",
+      descricao: "O Botafogo de Futebol e Regatas, popularmente conhecido como Botafogo, é um clube poliesportivo brasileiro com sede na cidade do Rio de Janeiro. Fundado em 12 de agosto de 1904, o Botafogo é um dos clubes mais tradicionais do Brasil e possui uma grande torcida.",
+      link: "https://pt.wikipedia.org/wiki/Botafogo_de_Futebol_e_Regatas",
+      competicoes: "Atualmente o Botafogo disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Carioca.",
+      tags: "Estrela Solitária, Glorioso"
+  },
+
+
+{
+    clube: "Internacional",
+    descricao: "O Sport Club Internacional, popularmente conhecido como Internacional, é um clube poliesportivo brasileiro com sede na cidade de Porto Alegre, no estado do Rio Grande do Sul. Fundado em 4 de abril de 1909, o Internacional é um dos clubes mais populares do Brasil e possui uma vasta história marcada por conquistas nacionais e internacionais.",
+    link: "https://pt.wikipedia.org/wiki/Sport_Club_Internacional",
+    competicoes: "Atualmente o Internacional disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Gaúcho.",
+    tags: "Colorado Guri Beira-Rio  inter Inter"
+},
+
+{
+    clube: "Grêmio",
+    descricao: "O Grêmio Foot-Ball Porto Alegrense, popularmente conhecido como Grêmio, é um clube poliesportivo brasileiro com sede na cidade de Porto Alegre, no estado do Rio Grande do Sul. Fundado em 15 de fevereiro de 1903, o Grêmio é um dos clubes mais tradicionais do Brasil e possui uma grande torcida.",
+    link: "https://pt.wikipedia.org/wiki/Grêmio_Foot-Ball_Porto_Alegrense",
+    competicoes: "Atualmente o Grêmio disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Gaúcho.",
+    tags: "Imortal, Tricolor, Arena"
+},
+
+{
+    clube: "Juventude",
+    descricao: "O Esporte Clube Juventude, popularmente conhecido como Juventude, é um clube poliesportivo brasileiro com sede na cidade de Caxias do Sul, no estado do Rio Grande do Sul. Fundado em 14 de setembro de 1913, o Juventude é um dos clubes mais tradicionais do Rio Grande do Sul e possui uma grande torcida.",
+    link: "https://pt.wikipedia.org/wiki/Esporte_Clube_Juventude",
+    competicoes: "Atualmente o Juventude disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Gaúcho.",
+    tags: "Alviverde, Jaconero"
+},
+
+{
+    clube: "Criciúma",
+    descricao: "O Criciúma Esporte Clube, popularmente conhecido como Criciúma, é um clube poliesportivo brasileiro com sede na cidade de Criciúma, no estado do Rio Grande do Sul. Fundado em 13 de maio de 1948, o Criciúma é um dos clubes mais tradicionais do estado e possui uma grande torcida.",
+    link: "https://pt.wikipedia.org/wiki/Criciúma_Esporte_Clube",
+    competicoes: "Atualmente o Criciúma disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Catarinense.",
+    tags: "Tigre, Carvoeiro"
+},
+
+{
+  clube: "Bahia",
+  descricao: "O Esporte Clube Bahia, popularmente conhecido como Bahia, é um clube poliesportivo brasileiro com sede na cidade de Salvador, no estado da Bahia. Fundado em 14 de janeiro de 1931, o Bahia é um dos clubes mais populares do Brasil e possui uma vasta história marcada por conquistas nacionais e internacionais.",
+  link: "https://pt.wikipedia.org/wiki/Esporte_Clube_Bahia",
+  competicoes: "Atualmente o Bahia disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Baiano.",
+  tags: "Esquadrão de Aço, Tricolor de Aço"
+},
+
+{
+  clube: "Vitória",
+  descricao: "O Esporte Clube Vitória, popularmente conhecido como Vitória, é um clube poliesportivo brasileiro com sede na cidade de Salvador, no estado da Bahia. Fundado em 9 de maio de 1899, o Vitória é um dos clubes mais tradicionais do Brasil e possui uma grande torcida.",
+  link: "https://pt.wikipedia.org/wiki/Esporte_Clube_Vitória",
+  competicoes: "Atualmente o Vitória disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Baiano.",
+  tags: "Leão, Rubro-Negro"
+},
+
+{
+  clube: "Cuiabá",
+  descricao: "O Cuiabá Esporte Clube, popularmente conhecido como Cuiabá, é um clube de futebol brasileiro com sede na cidade de Cuiabá, no estado do Mato Grosso. Fundado em 1 de abril de 2001, o Cuiabá é um clube de ascensão recente no futebol brasileiro.",
+  link: "https://pt.wikipedia.org/wiki/Cuiabá_Esporte_Clube",
+  competicoes: "Atualmente o Cuiabá disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Mato-Grossense.",
+  tags: "Dourado"
+},
+
+{
+  clube: "Fortaleza",
+  descricao: "O Fortaleza Esporte Clube, popularmente conhecido como Fortaleza, é um clube poliesportivo brasileiro com sede na cidade de Fortaleza, no estado do Ceará. Fundado em 18 de outubro de 1918, o Fortaleza é um dos clubes mais tradicionais do Nordeste e possui uma grande torcida.",
+  link: "https://pt.wikipedia.org/wiki/Fortaleza_Esporte_Clube",
+  competicoes: "Atualmente o Fortaleza disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Cearense.",
+  tags: "Leão do Pici, Tricolor de Aço"
+},
+
+{
+  clube: "Cruzeiro",
+  descricao: "O Cruzeiro Esporte Clube, popularmente conhecido como Cruzeiro, é um clube poliesportivo brasileiro com sede na cidade de Belo Horizonte, em Minas Gerais. Fundado em 2 de janeiro de 1921, o Cruzeiro é um dos clubes mais populares do Brasil e possui uma vasta história marcada por conquistas nacionais e internacionais.",
+  link: "https://pt.wikipedia.org/wiki/Cruzeiro_Esporte_Clube",
+  competicoes: "Atualmente o Cruzeiro disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Mineiro.",
+  tags: "Raposa, Celeste"
+},
+
+{
+  clube: "Atlético Mineiro",
+  descricao: "O Clube Atlético Mineiro, popularmente conhecido como Atlético Mineiro ou Galo, é um clube poliesportivo brasileiro com sede na cidade de Belo Horizonte, em Minas Gerais. Fundado em 25 de março de 1908, o Atlético é um dos clubes mais populares do Brasil e possui uma vasta história marcada por conquistas nacionais e internacionais.",
+  link: "https://pt.wikipedia.org/wiki/Clube_Atlético_Mineiro",
+  competicoes: "Atualmente o Atlético Mineiro disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Mineiro.",
+  tags: "Galo, Alvinegro"
+},
+
+{
+  clube: "Atlético Goianiense",
+  descricao: "O Atlético Goianiense, popularmente conhecido como Dragão, é um clube de futebol brasileiro com sede na cidade de Goiânia, no estado de Goiás. Fundado em 16 de janeiro de 1933, o Atlético Goianiense é um dos clubes mais tradicionais de Goiás e possui uma grande torcida.",
+  link: "https://pt.wikipedia.org/wiki/Atlético_Goianiense",
+  competicoes: "Atualmente o Atlético Goianiense disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Goiano.",
+  tags: "Dragão"
+},
+
+{
+  clube: "Athletico Paranaense",
+  descricao: "O Club Athletico Paranaense, popularmente conhecido como Athletico Paranaense ou Athletico, é um clube poliesportivo brasileiro com sede na cidade de Curitiba, no estado do Paraná. Fundado em 26 de março de 1924, o Athletico é um dos clubes mais tradicionais do Paraná e possui uma grande torcida.",
+  link: "https://pt.wikipedia.org/wiki/Club_Athletico_Paranaense",
+  competicoes: "Atualmente o Athletico Paranaense disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Paranaense.",
+  tags: "Furacão, Rubro-Negro"
+},
+
+{
+  clube: "Red Bull Bragantino",
+  descricao: "O Red Bull Bragantino, popularmente conhecido como Bragantino, é um clube de futebol brasileiro com sede na cidade de Bragança Paulista, em São Paulo. Fundado em 1928, o clube passou por uma reestruturação significativa após a aquisição pela empresa Red Bull em 2019, tornando-se uma das principais forças do futebol brasileiro.",
+  link: "https://pt.wikipedia.org/wiki/Red_Bull_Bragantino",
+  competicoes: "Atualmente o Red Bull Bragantino disputa: O Campeonato Brasileiro, a Copa do Brasil e o Campeonato Paulista.",
+  tags: "Massa Bruta, Toro Loko"
+}
 
 ];
 
